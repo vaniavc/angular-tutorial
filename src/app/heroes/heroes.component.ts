7ae33180da2c4867a6de4ab1870df601f3b32d29
vania chandra <vania@bagubagu.com>
2018-08-15 19:18:29 +0700

    4. Displaying a List

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
 
 @Component({
     selector: 'app-heroes',
@@ -7,11 +9,19 @@ import { Hero } from '../hero';
     styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-    /*hero = "Windstrom";*/
+    /* hero = "Windstrom";*/
+    /* array 
     hero: Hero = {
         id: 1,
         name: 'Windstrom'
-    };
+    }; */
+    heroes = HEROES;
+    selectedHero: Hero;
+
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
+
 
     constructor() {
     }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: "A" },
+    { id: 12, name: "B" },
+    { id: 13, name: "C" },
+    { id: 14, name: "D" },
+    { id: 15, name: "E" },
+    { id: 16, name: "F" },
+    { id: 17, name: "G" },
+    { id: 18, name: "H" },
+    { id: 19, name: "I" },
+    { id: 20, name: "J" }
+];
