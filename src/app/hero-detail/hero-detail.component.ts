231180343e8546d96d886cad3a41e50c5dd10388
vania chandra <vania@bagubagu.com>
2018-08-21 17:21:27 +0700

    8.HTTP

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
+import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
+import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
 
@@ -12,7 +12,7 @@ import { HeroService } from '../hero.service';
     styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-    hero: Hero;
+    @Input() hero: Hero;
 
     constructor(
         private route: ActivatedRoute,
@@ -33,5 +33,11 @@ export class HeroDetailComponent implements OnInit {
     goBack(): void {
         this.location.back();
     }
+
+    save(): void {
+        this.heroService.updateHero(this.hero)
+            .subscribe(() => this.goBack());
+    }
+
 
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
 // import { HEROES } from '../mock-heroes';
@@ -29,5 +30,19 @@ export class HeroesComponent implements OnInit {
     getHeroes(): void {
         this.heroService.getHeroes()
             .subscribe(heroes => this.heroes = heroes);
+    }
+
+    add(name: string): void {
+        name = name.trim();
+        if (!name) { return; }
+        this.heroService.addHero({ name } as Hero)
+            .subscribe(hero => {
+                this.heroes.push(hero);
+            });
+    }
+
+    delete(hero: Hero): void {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        this.heroService.deleteHero(hero).subscribe();
     }
 }
